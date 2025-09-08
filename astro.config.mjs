b330dfe8a014759f0ceb45612bcd517d2a7b4fb6
gramaphenia <dicoratojuanpablo@gmail.com>
2025-09-08 17:56:55 -0300

    feat: integrar Vercel Analytics y ajustar precios en la sección de productos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import vercel from '@astrojs/vercel/serverless';
+ 
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
